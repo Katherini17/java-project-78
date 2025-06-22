f8268af111990792f97d2eceeb92084aa197c850
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-06-22 19:18:20 +0300

    fix: Fix build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,15 +37,16 @@ sonar {
     }
 }
 
+jacoco {
+    toolVersion = "0.8.13"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+
 tasks.test {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
-}
-
-jacoco {
-    toolVersion = "0.8.13"
 }
 
 tasks.jacocoTestReport {
