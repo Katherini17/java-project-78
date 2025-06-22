83df6943f3d70941ff41a31aa2e318fe6a41e295
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-06-22 18:44:18 +0300

    Add Validator class to validate data and StringSchema class to validate strings

---
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -0,0 +1,47 @@
+package hexlet.code.schemas;
+
+import lombok.Setter;
+
+@Setter
+public class StringSchema {
+    private boolean isRequired;
+    private int minStringLength;
+    private String subString;
+
+    public StringSchema() {
+        this.isRequired = false;
+        this.minStringLength = 0;
+        this.subString = "";
+    }
+
+    public StringSchema required() {
+        setRequired(true);
+        return this;
+    }
+
+    public StringSchema minLength(int length) {
+        setMinStringLength(length);
+        if (length != 0) {
+            setRequired(true);
+        }
+        return this;
+    }
+
+    public StringSchema contains(String text) {
+        if (text != null && !text.isEmpty()) {
+            setRequired(true);
+            setSubString(text.toLowerCase());
+        }
+
+        setSubString(text);
+        return this;
+    }
+
+    public boolean isValid(String text) {
+        if (text == null) {
+            return !isRequired;
+        }
+
+        return text.length() >= minStringLength && text.toLowerCase().contains(subString);
+    }
+}
---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,46 @@
+package hexlet.code;
+
+import hexlet.code.schemas.StringSchema;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    public void testStringValidator() {
+        Validator validator = new Validator();
+        StringSchema schema = validator.string();
+
+        String emptyText = "";
+        String exampleText1 = "Example text";
+        String exampleText2 = "Text";
+        String exampleText3 = "The longest example";
+        String subString1 = "example";
+        String subString2 = "longest";
+
+        assertTrue(schema.isValid(emptyText));
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(exampleText1));
+
+        schema.required();
+
+        assertFalse(schema.isValid(exampleText2));
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(exampleText1));
+
+        schema.minLength(5);
+
+        assertFalse(schema.isValid(exampleText2));
+        assertTrue(schema.isValid(exampleText1));
+
+        schema.contains(subString1);
+
+        assertFalse(schema.isValid(exampleText2));
+        assertTrue(schema.isValid(exampleText1));
+
+
+        schema.minLength(14).contains(subString2);
+
+        assertFalse(schema.isValid(exampleText1));
+        assertTrue(schema.isValid(exampleText3));
+    }
+}
