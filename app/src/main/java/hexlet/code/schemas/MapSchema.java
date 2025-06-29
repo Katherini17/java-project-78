4e5c48dee072e2cdfad7379b1b9ccd83386effc1
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-06-29 19:30:24 +0300

    feat: Add shape() method MapSchema class for complicated Map validation

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,18 +3,37 @@ package hexlet.code.schemas;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public class MapSchema extends BaseSchema<Map<Object, Object>> {
+public class MapSchema<K, V> extends BaseSchema<Map<K, V>> {
 
     public MapSchema required() {
-        Predicate<Map<Object, Object>> testRequired = value -> value != null;
+        Predicate<Map<K, V>> testRequired = value -> value != null;
         addCondition("required", testRequired);
 
         return this;
     }
 
     public MapSchema sizeOf(int size) {
-        Predicate<Map<Object, Object>> testSizeOf = value -> value.size() >= size;
+        Predicate<Map<K, V>> testSizeOf = value -> value.size() == size;
         addCondition("sizeOf", testSizeOf);
+
+        return this;
+    }
+
+    public MapSchema shape(Map<K, BaseSchema<V>>  schemas) {
+        Predicate<Map<K, V>> testShape = complicatedMap -> {
+            return schemas.keySet()
+                    .stream()
+                    .map(key -> {
+
+                        V value = complicatedMap.get(key);
+                        var schema = schemas.get(key);
+
+                        return schema.isValid(value);
+                    })
+                    .allMatch(value -> value);
+        };
+
+        addCondition("shape", testShape);
 
         return this;
     }
